f2745cda03a43ba001f5b0fffff22977d4c4f78a
YoProgrammer <yuttasakcom@gmail.com>
2017-07-08 15:50:47 +0700

    Updated for support mongo hostname 'localhost' or 'mongo'

--- libs/config.js
+++ libs/config.js
@@ -1,9 +1,13 @@
 var nconf = require('nconf');
 
+var config = (process.env.ENV_IN === 'docker') ?
+    '/docker.config.json' :
+    '/config.json';
+
 nconf.argv()
 	.env()
 	.file({
-		file: process.cwd() + '/config.json'
+		file: process.cwd() + config
 	});
 
 module.exports = nconf;
